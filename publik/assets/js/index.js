4ad84f4ced13a361ec77869c6292932e7c61f1ac
Anton Petrenko <antoni.petrenko.dpua@gmail.com>
2020-07-21 15:59:20 +0300

    create animations

--- publik/assets/js/index.js
+++ publik/assets/js/index.js
@@ -1,7 +1,29 @@
 window.addEventListener("load", (e) => {
   const pages = [...document.querySelectorAll(".page")];
-  const madal= document.querySelector(".modal__container");
+  const madal = document.querySelector(".modal__container");
   const loader = document.querySelector(".loader");
+
+  const handleButton = (visiblePage, e) => {
+    visiblePage.classList.toggle("active");
+    visiblePage.addEventListener(
+      "transitionend",
+      () => {
+        visiblePage.nextElementSibling.classList.add("active");
+        if (visiblePage.querySelector("video")) {
+          visiblePage.querySelector("video").pause();
+        }
+        if (visiblePage.nextElementSibling.querySelector("video")) {
+          visiblePage.nextElementSibling
+            .querySelector("video")
+            .play();
+        }
+      },
+      {
+        once: true,
+      }
+    );
+  };
+
   const handleStartTouch = (e) => (startX = e.touches[0].clientX);
 
   const handleEndTouch = (limit) => {
@@ -13,15 +35,26 @@ window.addEventListener("load", (e) => {
         const direction = change < 0 ? "right" : "left";
         const half = screen.width / 3;
         const visiblePage = this;
+
         switch (direction) {
           case "right":
             if (Math.abs(change) > half && visiblePage.previousElementSibling) {
               //to the right
-              pages.forEach((page) => page.classList.remove("active"));
+              this.classList.toggle("active");
               visiblePage.addEventListener(
                 "transitionend",
                 () => {
                   visiblePage.previousElementSibling.classList.add("active");
+                  if (visiblePage.querySelector("video")) {
+                    visiblePage.querySelector("video").pause();
+                  }
+                  if (
+                    visiblePage.previousElementSibling.querySelector("video")
+                  ) {
+                    visiblePage.previousElementSibling
+                      .querySelector("video")
+                      .play();
+                  }
                 },
                 {
                   once: true,
@@ -37,6 +70,14 @@ window.addEventListener("load", (e) => {
                 "transitionend",
                 () => {
                   visiblePage.nextElementSibling.classList.add("active");
+                  if (visiblePage.querySelector("video")) {
+                    visiblePage.querySelector("video").pause();
+                  }
+                  if (visiblePage.nextElementSibling.querySelector("video")) {
+                    visiblePage.nextElementSibling
+                      .querySelector("video")
+                      .play();
+                  }
                 },
                 {
                   once: true,
@@ -47,15 +88,21 @@ window.addEventListener("load", (e) => {
           default:
             break;
         }
-        setTimeout(() => (inThrottle = false), limit);
+
+        setTimeout(() => {
+          inThrottle = false;
+        }, limit);
       }
     };
   };
 
   pages.forEach((page) => {
     let startX;
-    page.addEventListener("touchstart", handleStartTouch,false);
+    page.addEventListener("touchstart", handleStartTouch, false);
     page.addEventListener("touchend", handleEndTouch(1000), false);
+    page
+      .querySelector("button")
+      .addEventListener("click", handleButton.bind(null, page), false);
   });
 
   full_screen_submit.addEventListener("click", async (e) => {
